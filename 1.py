04d1d5d02b46e644a72a4e648d674f6c92ff7a9b
kolyan3445 <55030059+kolyan3445@users.noreply.github.com>
2026-01-09 03:03:00 +0400

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-from math import *
-
-x = float(input('Введите значение x: '))
-z1 = (sin(4 * x)) / (1 + cos(4 * x)) * (cos(2 * x) / (1 + cos(2 * x)))
-print("{0:.2f} {1:.4f} ".format(x, z1))
-z2 = cos((3/2) * pi - x) / (sin(3/2) * pi - x)
-print("{0:.2f} {1:.4f}".format(x, z2))
